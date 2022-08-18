8520fa99eca40a9fb3f764cf15f30d5d8940fab6
Germán Estévez <111249026+TheGims@users.noreply.github.com>
2022-08-18 16:46:02 -0300

    Update 0.08.04

--- Test.py
+++ Test.py
@@ -2,5 +2,6 @@ import tkinter
 ventana = tkinter.Tk()
 ventana.geometry("400x300")
 etiqueta = tkinter.Label(ventana, text = "Hola Mundo", bg = "blue")
-etiqueta.pack(side = tkinter.BOTTOM)
+#etiqueta.pack(side = tkinter.BOTTOM)
+etiqueta.pack(fill = tkinter.X)
 ventana.mainloop()
