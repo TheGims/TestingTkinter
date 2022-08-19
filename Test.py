d518ae18cb1b7e15d9ee24e16020305007645ee5
Germán Estévez <111249026+TheGims@users.noreply.github.com>
2022-08-19 17:32:09 -0300

    Update 0.8:51

--- Test.py
+++ Test.py
@@ -1,7 +1,6 @@
 import tkinter
 ventana = tkinter.Tk()
 ventana.geometry("400x300")
-etiqueta = tkinter.Label(ventana, text = "Hola Mundo", bg = "blue")
-#etiqueta.pack(side = tkinter.BOTTOM)
-etiqueta.pack(fill = tkinter.X)
+boton1 = tkinter.Button(ventana, text = "Presiona")
+boton1.pack()
 ventana.mainloop()
