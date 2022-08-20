63c76ab3a1eafb5dca64a0e74adf97ea6619405b
Germán Estévez <111249026+TheGims@users.noreply.github.com>
2022-08-20 07:13:05 -0300

    Udate 0.11.54

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 import tkinter
 ventana = tkinter.Tk()
 ventana.geometry("400x300")
-boton1 = tkinter.Button(ventana, text = "Presiona")
+def saludo(nombre):
+        print("Hola " + nombre)
+boton1 = tkinter.Button(ventana, text = "Presiona", command = lambda: saludo("python"))
 boton1.pack()
 ventana.mainloop()
