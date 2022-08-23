8005ede72b29870bd34830421555143d9382a190
Germán Estévez <111249026+TheGims@users.noreply.github.com>
2022-08-23 18:16:17 -0300

    Update 1.0

--- Test.py
+++ Test.py
@@ -1,13 +1,10 @@
 import tkinter
 ventana = tkinter.Tk()
 ventana.geometry("400x300")
-cajaTexto = tkinter.Entry(ventana)
-cajaTexto.pack()
-etiqueta = tkinter.Label(ventana)
-etiqueta.pack()
-def textoDeLaCaja():
-        text20 = cajaTexto.get()
-        etiqueta["text"] = text20
-boton1 = tkinter.Button(ventana, text = "click", command = textoDeLaCaja)
-boton1.pack()
+boton1 = tkinter.Button(ventana, text = "boton1", width = 10, height = 5)
+boton2 = tkinter.Button(ventana, text = "boton2", width = 10, height = 5)
+boton3 = tkinter.Button(ventana, text = "boton3", width = 10, height = 5)
+boton1.grid(row = 0, column = 0)
+boton2.grid(row = 1, column = 1)
+boton3.grid(row = 2, column = 2)
 ventana.mainloop()
